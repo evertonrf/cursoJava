eed9f812097fe55f9c2af9bd9df7195cbcefec9f
EVERTON RUFINO DA SILVA <64816217+evertonrf@users.noreply.github.com>
2023-07-31 10:04:07 -0300

    Initial commit

---
+++ java.java
@@ -0,0 +1,33 @@
+class Myclass{
+    int x;
+}
+Myclass() {
+    x = 10;
+}
+class consDemo{
+ public static void public static void main(strin[] args){
+     Myclass t1 = new Myclass(); Myclass t2 = new Myclass();
+
+     System.out.println(t1.x + " " + t2.x)
+     
+ }
+}
+
+
+ class Retangulo extends DuasDimensoes {
+
+              boolean ehQuadrado() {
+
+                      if(largura == altura) return true;
+
+                      return false;
+
+               }
+
+              double area() {
+
+                   return largura * altura;
+
+              }
+
+            }
